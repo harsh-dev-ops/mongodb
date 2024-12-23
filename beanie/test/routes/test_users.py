71dd2e3ee73933b74751c3369cbead52e6e869f1
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-23 15:51:25 +0530

    fixed static user data bug

--- beanie/test/routes/test_users.py
+++ beanie/test/routes/test_users.py
@@ -1,18 +1,21 @@
 from httpx import AsyncClient
 import pytest
 
-enpoint_url = "/users"
+NEW_USER_DATA = {}
 
-sample_user_data = {
-        "name": "sample",
-        "email": "sample@sample.com",
-        "superuser": True,
-        "uid": "2c856c42-2144-4df1-9c46-e3e33f432a84"
-}
+def update_user_data(data: dict):
+    for key, value in data.items():
+        NEW_USER_DATA[key] = value
 
 
 class TestUserApi:
     enpoint_url = "/users"
+    create_user_data = {
+            "name": "sample",
+            "email": "sample@sample.com",
+            "superuser": True,
+            "uid": "2c856c42-2144-4df1-9c46-e3e33f432a84"
+    }
 
     @pytest.mark.asyncio
     async def test_get_users(self, client: AsyncClient):
@@ -22,18 +25,19 @@ class TestUserApi:
 
     @pytest.mark.asyncio
     async def test_create_user(self, client: AsyncClient):
-        response = await client.post(url=f"{self.enpoint_url}", json=sample_user_data)
-        assert response.json()['uid'] == sample_user_data['uid']
+        response = await client.post(url=f"{self.enpoint_url}", json=self.create_user_data)
+        update_user_data(response.json())
+        assert response.json()['uid'] == self.create_user_data['uid']
         assert response.status_code == 201
 
     @pytest.mark.asyncio
     async def test_get_user(self, client: AsyncClient):
-        uid = sample_user_data['uid']
+        uid = NEW_USER_DATA['uid']
         response = await client.get(url=f"{self.enpoint_url}/{uid}")
         assert response.status_code == 200
 
     @pytest.mark.asyncio
     async def test_delete_user(self, client: AsyncClient):
-        uid = sample_user_data['uid']
+        uid = NEW_USER_DATA['uid']
         response = await client.delete(url=f"{self.enpoint_url}/{uid}")
         assert response.status_code == 204
